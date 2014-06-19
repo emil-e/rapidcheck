6293425ce882e4ad4743722fa051d5fd746146eb
Emil Eriksson <shadewind@gmail.com>
2014-06-19 17:06:29 +0200

    Changed quantifier so that order of argument generation is defined

--- include/rapidcheck/detail/Quantifier.hpp
+++ include/rapidcheck/detail/Quantifier.hpp
@@ -5,13 +5,39 @@
 namespace rc {
 namespace detail {
 
+template<typename Callable, typename ReturnType, typename ...Args>
+struct Invoker;
+
+template<typename Callable, typename ReturnType>
+struct Invoker<Callable, ReturnType>
+{
+    static ReturnType invoke(const Callable &callable) { return callable(); }
+};
+
+template<typename Callable,
+         typename ReturnType,
+         typename Arg,
+         typename ...Args>
+struct Invoker<Callable, ReturnType, Arg, Args...>
+{
+    static ReturnType
+    invoke(const Callable &callable)
+    {
+        auto arg(pick<typename std::decay<Arg>::type>());
+        auto curried = [&] (typename std::decay<Args>::type &...args) {
+            callable(std::forward<Arg>(arg), std::forward<Args>(args)...);
+        };
+        return Invoker<decltype(curried), ReturnType, Args...>::invoke(curried);
+    }
+};
+
 template<typename Functor, typename Ret, typename ...Args>
 FunctorHelper<Ret (Functor::*)(Args...) const>::FunctorHelper(Functor functor)
     : m_functor(std::move(functor)) {}
 
 template<typename Functor, typename Ret, typename ...Args>
 Ret FunctorHelper<Ret (Functor::*)(Args...) const>::operator()() const
-{ return m_functor(pick<typename std::decay<Args>::type>()...); }
+{ return Invoker<Functor, Ret, Args...>::invoke(m_functor); }
 
 template<typename Callable>
 Quantifier<Callable>::Quantifier(Callable callable)
@@ -22,7 +48,7 @@ Quantifier<Ret (*)(Args...)>::Quantifier(Function function) : m_function(functio
 
 template<typename Ret, typename ...Args>
 Ret Quantifier<Ret (*)(Args...)>::operator()() const
-{ return m_function(pick<typename std::decay<Args>::type>()...); }
+{ return Invoker<Function, Ret, Args...>::invoke(m_function); }
 
 } // namespace detail
 } // namespace rc
