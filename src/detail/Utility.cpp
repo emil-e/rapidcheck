802b51a2a14b9044ebbbff09c91ad6e990911b9c
Emil Eriksson <shadewind@gmail.com>
2015-02-04 21:49:21 +0100

    demangle: Don't rely on buffer length for content length

--- src/detail/Utility.cpp
+++ src/detail/Utility.cpp
@@ -11,9 +11,9 @@ std::string demangle(const char *name)
     std::string demangled(name);
     int status;
     std::size_t length;
-    char *buf = abi::__cxa_demangle(name, NULL, &length, &status);
+    char *buf = abi::__cxa_demangle(name, 0, 0, &status);
     if (status == 0)
-        demangled = std::string(buf, length);
+        demangled = std::string(buf);
     free(buf);
     return demangled;
 }
