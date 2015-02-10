b6ad4be13df6e74d7a351ffffe5edc03f0e94b40
Emil Eriksson <shadewind@gmail.com>
2015-02-11 00:25:34 +0100

    Use RC__ prefix for internal macros

--- include/rapidcheck/Assertions.h
+++ include/rapidcheck/Assertions.h
@@ -2,50 +2,51 @@
 
 #include "rapidcheck/detail/Results.h"
 
-#define RC_CONDITIONAL_RESULT(ResultType, condition, msg)              \
-    ::rc::detail::throwResultIf(                                       \
-        ::rc::detail::CaseResult::Type::ResultType,                    \
-        (condition),                                                   \
-        msg,                                                           \
-        __FILE__,                                                      \
+
+#define RC__CONDITIONAL_RESULT(ResultType, condition, msg)      \
+    ::rc::detail::throwResultIf(                                \
+        ::rc::detail::CaseResult::Type::ResultType,             \
+        (condition),                                            \
+        msg,                                                    \
+        __FILE__,                                               \
         __LINE__)
 
-#define RC_UNCONDITIONAL_RESULT(ResultType, msg)                       \
-    ::rc::detail::throwResult(                                         \
-        ::rc::detail::CaseResult::Type::ResultType,                    \
-        msg,                                                           \
-        __FILE__,                                                      \
+#define RC__UNCONDITIONAL_RESULT(ResultType, msg)       \
+    ::rc::detail::throwResult(                          \
+        ::rc::detail::CaseResult::Type::ResultType,     \
+        msg,                                            \
+        __FILE__,                                       \
         __LINE__)
 
 //! Fails the current test case unless the given condition is `true`.
 #define RC_ASSERT(condition)                    \
-    RC_CONDITIONAL_RESULT(Failure,              \
-                          !(condition),         \
-                          #condition)
+    RC__CONDITIONAL_RESULT(Failure,             \
+                           !(condition),        \
+                           #condition)
 
 //! Unconditionally fails the current test case with the given message.
 #define RC_FAIL(msg)                            \
-    RC_UNCONDITIONAL_RESULT(Failure, (msg))
+    RC__UNCONDITIONAL_RESULT(Failure, (msg))
 
 //! Succeed if the given condition is true.
 #define RC_SUCCEED_IF(condition)                \
-    RC_CONDITIONAL_RESULT(Success,              \
-                          (condition),          \
-                          #condition)
+    RC__CONDITIONAL_RESULT(Success,             \
+                           (condition),         \
+                           #condition)
 
 //! Unconditionally succeed with the given message.
 #define RC_SUCCEED(msg)                         \
-    RC_UNCONDITIONAL_RESULT(Success, (msg))
+    RC__UNCONDITIONAL_RESULT(Success, (msg))
 
 //! Discards the current test case if the given condition is false.
 #define RC_PRE(condition)                       \
-    RC_CONDITIONAL_RESULT(Discard,              \
-                          !(condition),         \
-                          #condition)
+    RC__CONDITIONAL_RESULT(Discard,             \
+                           !(condition),        \
+                           #condition)
 
 //! Discards the current test case with the given description.
 #define RC_DISCARD(msg)                         \
-    RC_UNCONDITIONAL_RESULT(Discard, (msg))
+    RC__UNCONDITIONAL_RESULT(Discard, (msg))
 
 namespace rc {
 namespace detail {
