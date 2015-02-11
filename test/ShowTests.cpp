caa380cf6a54b2981c082e91aba70a9e455c7456
Emil Eriksson <shadewind@gmail.com>
2015-02-11 19:44:29 +0100

    Fix some terminology in ShowTests.cpp

--- test/ShowTests.cpp
+++ test/ShowTests.cpp
@@ -7,7 +7,7 @@ namespace {
 
 struct Showable {};
 
-void show(Showable x, std::ostream &os) { os << "show(Showable)"; };
+void showValue(Showable x, std::ostream &os) { os << "showValue(Showable)"; };
 
 std::ostream &operator<<(std::ostream &os, Showable)
 {
@@ -30,17 +30,17 @@ struct NonShowable {};
 TEST_CASE("show<T>") {
     std::ostringstream os;
 
-    SECTION("uses show(...) overload if available") {
+    SECTION("uses showValue(...) overload if available") {
         show(Showable(), os);
-        REQUIRE(os.str() == "show(Showable)");
+        REQUIRE(os.str() == "showValue(Showable)");
     }
 
-    SECTION("tries to use operator<<(ostream...) if show(...) is not availble") {
+    SECTION("tries to use operator<<(ostream...) if showValue(...) is not availble") {
         show(Ostreamable(), os);
         REQUIRE(os.str() == "<< Ostreamable");
     }
 
-    SECTION("if neither operator<< or show(...) available, show as <\?\?\?>") {
+    SECTION("if neither operator<< or showValue(...) available, show as <\?\?\?>") {
         show(NonShowable(), os);
         REQUIRE(os.str() == "<\?\?\?>");
     }
