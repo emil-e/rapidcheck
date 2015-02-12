22cb8e83f15ad114a6876033f25171a17d93061d
Emil Eriksson <shadewind@gmail.com>
2015-02-12 22:01:00 +0100

    Split container type list macros into ordered/unordered

--- test/util/TypeListMacros.h
+++ test/util/TypeListMacros.h
@@ -22,18 +22,24 @@
 
 #define RC_NUMERIC_TYPES RC_REAL_TYPES, RC_INTEGRAL_TYPES
 
-#define RC_GENERIC_CONTAINERS(T)                            \
-    std::vector<T>,                                         \
-    std::deque<T>,                                          \
-    std::forward_list<T>,                                   \
-    std::list<T>,                                           \
-    std::set<T>,                                            \
-    std::map<T, T>,                                         \
-    std::multiset<T>,                                       \
-    std::multimap<T, T>,                                    \
-    std::unordered_set<T>,                                  \
-    std::unordered_map<T, T>,                               \
-    std::unordered_multiset<T>,                             \
+#define RC_ORDERED_CONTAINERS(T)                \
+    std::vector<T>,                             \
+    std::deque<T>,                              \
+    std::forward_list<T>,                       \
+    std::list<T>,                               \
+    std::set<T>,                                \
+    std::map<T, T>,                             \
+    std::multiset<T>,                           \
+    std::multimap<T, T>
+
+#define RC_UNORDERED_CONTAINERS(T)              \
+    std::unordered_set<T>,                      \
+    std::unordered_map<T, T>,                   \
+    std::unordered_multiset<T>,                 \
     std::unordered_multimap<T, T>
+
+#define RC_GENERIC_CONTAINERS(T)                            \
+    RC_ORDERED_CONTAINERS(T),                               \
+    RC_UNORDERED_CONTAINERS(T)
 
 #define RC_STRING_TYPES std::string, std::wstring
