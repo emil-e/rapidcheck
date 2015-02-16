c18a89a95b1a4e1b939ac6cea6d3ede905f21cbb
Emil Eriksson <shadewind@gmail.com>
2015-02-16 16:14:27 +0100

    Make seq::drop lazy

--- include/rapidcheck/seq/Transform.hpp
+++ include/rapidcheck/seq/Transform.hpp
@@ -2,17 +2,40 @@
 
 namespace rc {
 namespace seq {
+namespace detail {
+
+template<typename T>
+class DropSeq
+{
+public:
+    DropSeq(std::size_t n, Seq<T> seq)
+        : m_drop(n)
+        , m_seq(std::move(seq)) {}
+
+    Maybe<T> operator()()
+    {
+        while (m_drop > 0) {
+            if (!m_seq.next()) {
+                m_seq = Seq<T>();
+                m_drop = 0;
+                return Nothing;
+            }
+            m_drop--;
+        }
+
+        return m_seq.next();
+    }
+
+private:
+    std::size_t m_drop;
+    Seq<T> m_seq;
+};
+
+} // namespace detail
 
 template<typename T>
 Seq<T> drop(std::size_t n, Seq<T> seq)
-{
-    for (std::size_t i = 0; i < n; i++) {
-        if (!seq.next())
-            return Seq<T>();
-    }
-
-    return seq;
-}
+{ return makeSeq<detail::DropSeq<T>>(n, std::move(seq)); }
 
 } // namespace seq
 } // namespace rc
