17aac5e9c91851c77baf7a45264487724d86c863
Emil Eriksson <shadewind@gmail.com>
2015-02-16 16:24:17 +0100

    Some more tests for seq::drop

--- test/seq/TransformTests.cpp
+++ test/seq/TransformTests.cpp
@@ -4,7 +4,10 @@
 #include "rapidcheck/seq/Transform.h"
 #include "rapidcheck/seq/Create.h"
 
+#include "util/CopyGuard.h"
+
 using namespace rc;
+using namespace rc::test;
 
 TEST_CASE("seq::drop") {
     prop("drops the first N elements from the given seq",
@@ -14,5 +17,22 @@ TEST_CASE("seq::drop") {
              std::vector<int> rest(elements.begin() + start, elements.end());
              RC_ASSERT(seq::drop(n, seq::fromContainer(elements)) ==
                        seq::fromContainer(rest));
+         });
+
+    prop("copies are equal",
+         [] (const std::vector<int> &elements) {
+             std::size_t n = *gen::ranged<std::size_t>(0, elements.size() * 2);
+             std::size_t start = std::min(elements.size(), n);
+             const auto seq = seq::drop(n, seq::fromContainer(elements));
+             const auto copy = seq;
+             RC_ASSERT(seq == copy);
+         });
+
+    prop("does not copy items",
+         [] (std::vector<CopyGuard> elements) {
+             std::size_t n = *gen::ranged<std::size_t>(0, elements.size() * 2);
+             std::size_t start = std::min(elements.size(), n);
+             auto seq = seq::drop(n, seq::fromContainer(std::move(elements)));
+             while (seq.next());
          });
 }
