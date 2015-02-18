1e0998cf1d80b72eafd1639c839c86ac6f38a6ef
Emil Eriksson <shadewind@gmail.com>
2015-02-18 09:56:07 +0100

    Add clarifying comment for assertEqualCopies

--- test/util/SeqUtils.h
+++ test/util/SeqUtils.h
@@ -6,7 +6,7 @@ namespace rc {
 namespace test {
 
 //! Forwards Seq a random amount and copies it to see if it is equal to the
-//! original.
+//! original. Must not be infinite, of course.
 template<typename T>
 void assertEqualCopies(Seq<T> seq)
 {
