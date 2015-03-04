92d3866410b258f343aa250593d00a8ccbfacacc
Emil Eriksson <shadewind@gmail.com>
2015-03-04 23:12:39 +0100

    Clarify seq::mapcat docs

--- include/rapidcheck/seq/Transform.h
+++ include/rapidcheck/seq/Transform.h
@@ -45,7 +45,8 @@ Seq<T> join(Seq<Seq<T>> seqs);
 template<typename T, typename ...Ts>
 Seq<T> concat(Seq<T> seq, Seq<Ts> ...seqs);
 
-//! Maps the elements to `Seq`s and concatenates them into one `Seq`.
+//! Maps each tuple elements of the given to `Seq`s to further `Seq`s and
+//! concatenates them into one `Seq`. Sometimes called a "flat map".
 template<typename ...Ts, typename Mapper>
 Seq<typename std::result_of<Mapper(Ts...)>::type::ValueType>
 mapcat(Mapper &&mapper, Seq<Ts> ...seqs);
