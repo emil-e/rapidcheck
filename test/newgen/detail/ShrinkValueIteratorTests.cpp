c58fa5049557c4004a1e4e0f26b4366cab03e00b
Emil Eriksson <shadewind@gmail.com>
2015-03-28 19:20:42 +0100

    Add ShrinkValueIterator

---
+++ test/newgen/detail/ShrinkValueIteratorTests.cpp
@@ -0,0 +1,49 @@
+#include <catch.hpp>
+#include <rapidcheck-catch.h>
+
+#include "rapidcheck/Shrinkable.h"
+#include "rapidcheck/shrinkable/Create.h"
+#include "rapidcheck/newgen/detail/ShrinkValueIterator.h"
+
+#include "util/Predictable.h"
+
+using namespace rc;
+using namespace rc::newgen::detail;
+
+TEST_CASE("ShrinkValueIterator") {
+    prop("returns the value of shrinkable referenced by the underlying"
+         " iterator",
+         [](const std::vector<int> &elements) {
+             std::vector<Shrinkable<int>> shrinkables;
+             shrinkables.reserve(elements.size());
+             std::transform(
+                 begin(elements), end(elements), std::back_inserter(shrinkables),
+                 &shrinkable::just<const int &>);
+             std::vector<int> result(
+                 makeShrinkValueIterator(begin(shrinkables)),
+                 makeShrinkValueIterator(end(shrinkables)));
+             RC_ASSERT(result == elements);
+         });
+
+    prop("works with non-copyable elements",
+         [](const std::vector<NonCopyable> &elements) {
+             std::vector<Shrinkable<NonCopyable>> shrinkables;
+             shrinkables.reserve(elements.size());
+             std::transform(
+                 begin(elements), end(elements), std::back_inserter(shrinkables),
+                 [](const NonCopyable &x) {
+                     const auto value = x.value;
+                     const auto extra = x.extra;
+                     return shrinkable::lambda([=] {
+                         NonCopyable nc;
+                         nc.value = value;
+                         nc.extra = extra;
+                         return nc;
+                     });
+                 });
+             std::vector<NonCopyable> result(
+                 makeShrinkValueIterator(begin(shrinkables)),
+                 makeShrinkValueIterator(end(shrinkables)));
+             RC_ASSERT(result == elements);
+         });
+}
