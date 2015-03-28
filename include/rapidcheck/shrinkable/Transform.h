e1d9f7f6473476c9da658edefe08fa0f9b3b4a50
Emil Eriksson <shadewind@gmail.com>
2015-03-28 18:03:40 +0100

    Clarify doc comment about shrinkable::map

--- include/rapidcheck/shrinkable/Transform.h
+++ include/rapidcheck/shrinkable/Transform.h
@@ -5,7 +5,7 @@
 namespace rc {
 namespace shrinkable {
 
-//! Maps the given shrinkable using the given mapping callable.
+//! Maps the given shrinkable recursively using the given mapping callable.
 template<typename T, typename Mapper>
 Shrinkable<typename std::result_of<Mapper(T)>::type>
 map(Mapper &&mapper, Shrinkable<T> shrinkable);
