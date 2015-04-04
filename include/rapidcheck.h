2fa340c84699a01f66c0596ee65568a93ed9c977
Emil Eriksson <shadewind@gmail.com>
2015-04-04 18:02:42 +0200

    Add next gen headers to rapidcheck.h

--- include/rapidcheck.h
+++ include/rapidcheck.h
@@ -1,5 +1,27 @@
 #pragma once
 
+#include "rapidcheck/Seq.h"
+#include "rapidcheck/seq/Create.h"
+#include "rapidcheck/seq/Operations.h"
+#include "rapidcheck/seq/Transform.h"
+
+#include "rapidcheck/Shrinkable.h"
+#include "rapidcheck/shrinkable/Create.h"
+#include "rapidcheck/shrinkable/Operations.h"
+#include "rapidcheck/shrinkable/Transform.h"
+
+#include "rapidcheck/Gen.h"
+#include "rapidcheck/newgen/Arbitrary.h"
+#include "rapidcheck/newgen/Container.h"
+#include "rapidcheck/newgen/Create.h"
+#include "rapidcheck/newgen/Distinct.h"
+#include "rapidcheck/newgen/Exec.h"
+#include "rapidcheck/newgen/Numeric.h"
+#include "rapidcheck/newgen/Select.h"
+#include "rapidcheck/newgen/Text.h"
+#include "rapidcheck/newgen/Transform.h"
+#include "rapidcheck/newgen/Tuple.h"
+
 #include "rapidcheck/Check.h"
 #include "rapidcheck/Show.h"
 #include "rapidcheck/Assertions.h"
