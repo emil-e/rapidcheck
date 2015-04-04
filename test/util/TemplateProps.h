02fee397f67bc0de85ab88d9a0bbd579f853544d
Emil Eriksson <shadewind@gmail.com>
2015-04-04 18:26:10 +0200

    Add versions of TemplateProps.h for new framework

--- test/util/TemplateProps.h
+++ test/util/TemplateProps.h
@@ -23,6 +23,26 @@ void propConformsToEquals() {
 }
 
 template<typename T>
+void newpropConformsToEquals() {
+    newprop(
+        "copies are equal",
+        [] (const T &x) {
+            T copy(x);
+            RC_ASSERT(x == copy);
+            RC_ASSERT(copy == x);
+            RC_ASSERT(!(x != copy));
+            RC_ASSERT(!(copy != x));
+        });
+
+    newprop(
+        "== is the opposite of !=",
+        [] (const T &x1, const T &x2) {
+            RC_ASSERT((x1 == x2) == !(x1 != x2));
+            RC_ASSERT((x2 == x1) == !(x2 != x1));
+        });
+}
+
+template<typename T>
 void propConformsToOutputOperator() {
     prop("output equality reflects input equality",
          [] (const T &x1, const T &x2) {
@@ -34,15 +54,40 @@ void propConformsToOutputOperator() {
          });
 }
 
-#define PROP_REPLACE_MEMBER_INEQUAL(Type, Member)             \
-    prop("not equal if " #Member " not equal",                \
-         [] (const Type &x) {                                 \
-             auto other(x);                                   \
-             other.Member = *gen::distinctFrom(other.Member); \
-             RC_ASSERT(x != other);                           \
-             RC_ASSERT(other != x);                           \
-             RC_ASSERT(!(x == other));                        \
-             RC_ASSERT(!(other == x));                        \
-         })                                                   \
+template<typename T>
+void newpropConformsToOutputOperator() {
+    newprop(
+        "output equality reflects input equality",
+        [] (const T &x1, const T &x2) {
+            std::ostringstream s1;
+            s1 << x1;
+            std::ostringstream s2;
+            s2 << x2;
+            RC_ASSERT((s1.str() == s2.str()) == (x1 == x2));
+        });
+}
+
+#define PROP_REPLACE_MEMBER_INEQUAL(Type, Member)               \
+    prop("not equal if " #Member " not equal",                  \
+         [] (const Type &x) {                                   \
+             auto other(x);                                     \
+             other.Member = *gen::distinctFrom(other.Member);   \
+             RC_ASSERT(x != other);                             \
+             RC_ASSERT(other != x);                             \
+             RC_ASSERT(!(x == other));                          \
+             RC_ASSERT(!(other == x));                          \
+         })
+
+#define NEWPROP_REPLACE_MEMBER_INEQUAL(Type, Member)            \
+    newprop(                                                    \
+        "not equal if " #Member " not equal",                   \
+        [] (const Type &x) {                                    \
+            auto other(x);                                      \
+            other.Member = *newgen::distinctFrom(other.Member); \
+            RC_ASSERT(x != other);                              \
+            RC_ASSERT(other != x);                              \
+            RC_ASSERT(!(x == other));                           \
+            RC_ASSERT(!(other == x));                           \
+        })
 
 } // namespace rc
