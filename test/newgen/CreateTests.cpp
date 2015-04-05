ad6e0bacfd9c62ff061a3ad1e80d35db18ac9193
Emil Eriksson <shadewind@gmail.com>
2015-04-05 16:16:38 +0200

    Use GenParams in CreateTests

--- test/newgen/CreateTests.cpp
+++ test/newgen/CreateTests.cpp
@@ -4,13 +4,15 @@
 #include "rapidcheck/newgen/Create.h"
 
 #include "util/ArbitraryRandom.h"
+#include "util/GenUtils.h"
 
 using namespace rc;
+using namespace rc::test;
 
 TEST_CASE("newgen::just") {
-    prop("returns value without shrinks regardless of input",
-         [](int value, const Random &random, int size) {
-             RC_ASSERT(newgen::just(value)(random, size) ==
+    prop("returns value without shrinks regardless of params",
+         [](const GenParams &params, int value) {
+             RC_ASSERT(newgen::just(value)(params.random, params.size) ==
                        shrinkable::just(value));
          });
 }
