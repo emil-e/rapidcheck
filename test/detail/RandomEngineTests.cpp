905cec6a9d64cad7fadaab6c5f689e12a7e1f45d
Emil Eriksson <shadewind@gmail.com>
2015-04-05 09:05:41 +0200

    Move RandomEngineTests to new framework

--- test/detail/RandomEngineTests.cpp
+++ test/detail/RandomEngineTests.cpp
@@ -10,38 +10,41 @@ using namespace rc::detail;
 
 TEST_CASE("RandomEngine") {
     SECTION("nextAtom") {
-        prop("same seeds yields same sequences of numbers",
-             [] (RandomEngine::Seed seed) {
-                 RandomEngine r1(seed);
-                 RandomEngine r2(seed);
-                 for (int i = 0; i < ENOUGH; i++)
-                     RC_ASSERT(r1.nextAtom() == r2.nextAtom());
-             });
+        newprop(
+            "same seeds yields same sequences of numbers",
+            [] (RandomEngine::Seed seed) {
+                RandomEngine r1(seed);
+                RandomEngine r2(seed);
+                for (int i = 0; i < ENOUGH; i++)
+                    RC_ASSERT(r1.nextAtom() == r2.nextAtom());
+            });
 
-        prop("a copy of a randomEngine yields the same sequences of numbers as"
-             " the original",
-             [] (RandomEngine::Seed seed) {
-                 RandomEngine r1(seed);
-                 RandomEngine r2(r1);
-                 for (int i = 0; i < ENOUGH; i++)
-                     RC_ASSERT(r1.nextAtom() == r2.nextAtom());
-             });
+        newprop(
+            "a copy of a randomEngine yields the same sequences of numbers as"
+            " the original",
+            [] (RandomEngine::Seed seed) {
+                RandomEngine r1(seed);
+                RandomEngine r2(r1);
+                for (int i = 0; i < ENOUGH; i++)
+                    RC_ASSERT(r1.nextAtom() == r2.nextAtom());
+            });
 
-        prop("different seeds yields different sequences of numbers",
-             [] {
-                 auto s1 = *gen::arbitrary<RandomEngine::Seed>();
-                 auto s2 = *gen::distinctFrom(s1);
+        newprop(
+            "different seeds yields different sequences of numbers",
+            [] {
+                auto s1 = *newgen::arbitrary<RandomEngine::Seed>();
+                auto s2 = *newgen::distinctFrom(s1);
 
-                 RandomEngine r1(s1);
-                 RandomEngine r2(s2);
-                 std::vector<RandomEngine::Atom> atoms1;
-                 std::vector<RandomEngine::Atom> atoms2;
-                 for (int i = 0; i < ENOUGH; i++) {
-                     atoms1.push_back(r1.nextAtom());
-                     atoms2.push_back(r2.nextAtom());
-                 }
+                RandomEngine r1(s1);
+                RandomEngine r2(s2);
+                std::vector<RandomEngine::Atom> atoms1;
+                std::vector<RandomEngine::Atom> atoms2;
+                for (int i = 0; i < ENOUGH; i++) {
+                    atoms1.push_back(r1.nextAtom());
+                    atoms2.push_back(r2.nextAtom());
+                }
 
-                 RC_ASSERT(atoms1 != atoms2);
-             });
+                RC_ASSERT(atoms1 != atoms2);
+            });
     }
 }
