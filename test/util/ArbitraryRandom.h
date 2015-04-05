12ee10ac9c6728a3589b3521f789d1f4902d11e5
Emil Eriksson <shadewind@gmail.com>
2015-04-05 09:03:21 +0200

    Add NewArbitrary for Random

--- test/util/ArbitraryRandom.h
+++ test/util/ArbitraryRandom.h
@@ -23,6 +23,30 @@ public:
     }
 };
 
+
+// This will only generate Randoms with no next() called.
+template<>
+struct NewArbitrary<Random>
+{
+    static Gen<Random> arbitrary()
+    {
+        return newgen::map(
+            newgen::pair(newgen::arbitrary<Random::Key>(),
+                         newgen::arbitrary<std::vector<bool>>()),
+            [](const std::pair<Random::Key, std::vector<bool>> &p) {
+                Random random(p.first);
+                for (bool x : p.second) {
+                    if (!x)
+                        random.split();
+                    else
+                        random = random.split();
+                }
+                return random;
+            });
+    }
+};
+
+
 namespace test {
 
 // This will also generate Randoms where next() has been called
