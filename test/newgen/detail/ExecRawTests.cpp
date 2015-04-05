fb7bec2c04663a19f7b9858c1a532c1d37698a22
Emil Eriksson <shadewind@gmail.com>
2015-04-05 15:14:43 +0200

    Move ExecRawTests to new framework

--- test/newgen/detail/ExecRawTests.cpp
+++ test/newgen/detail/ExecRawTests.cpp
@@ -74,124 +74,128 @@ TEST_CASE("execRaw") {
         REQUIRE(mappedShrinkable == tupleShrinkable);
     }
 
-    prop("traversing a random path through the shrink tree yields the expected"
-         " values",
-         []{
-             const auto shrinkable = testExecGen<5>()(Random(), 0);
-             const auto path = *gen::collection<std::vector<bool>>(
-                 gen::arbitrary<bool>());
+    newprop(
+        "traversing a random path through the shrink tree yields the expected"
+        " values",
+        []{
+            const auto shrinkable = testExecGen<5>()(Random(), 0);
+            const auto path = *newgen::container<std::vector<bool>>(
+                newgen::arbitrary<bool>());
 
-             std::vector<int> accepted = shrinkable.value().first;
-             auto acceptedShrinkable = shrinkable;
-             auto shrinks = shrinkable.shrinks();
-             int i = 0;
-             int x = 5;
-             for (bool accept : path) {
-                 if (i >= accepted.size()) {
-                     RC_ASSERT(!shrinks.next());
-                     break;
-                 }
+            std::vector<int> accepted = shrinkable.value().first;
+            auto acceptedShrinkable = shrinkable;
+            auto shrinks = shrinkable.shrinks();
+            int i = 0;
+            int x = 5;
+            for (bool accept : path) {
+                if (i >= accepted.size()) {
+                    RC_ASSERT(!shrinks.next());
+                    break;
+                }
 
-                 auto shrink = *shrinks.next();
-                 auto actual = shrink.value().first;
+                auto shrink = *shrinks.next();
+                auto actual = shrink.value().first;
 
-                 auto expected = accepted;
-                 expected[i] = --x;
-                 // First element determines the size
-                 expected.resize(expected[0] + 1);
-                 RC_ASSERT(actual == expected);
-                 if (accept) {
-                     accepted = expected;
-                     acceptedShrinkable = shrink;
-                     shrinks = shrink.shrinks();
-                 }
+                auto expected = accepted;
+                expected[i] = --x;
+                // First element determines the size
+                expected.resize(expected[0] + 1);
+                RC_ASSERT(actual == expected);
+                if (accept) {
+                    accepted = expected;
+                    acceptedShrinkable = shrink;
+                    shrinks = shrink.shrinks();
+                }
 
-                 if (x == 0) {
-                     x = 5;
-                     i++;
-                 }
-             }
-         });
+                if (x == 0) {
+                    x = 5;
+                    i++;
+                }
+            }
+        });
 
-    prop("the number of shrinks is never never more than the sum of the number"
-         " of shrinks for the requested values",
-         []{
-             const auto shrinkable = testExecGen<5>()(Random(), 0);
+    newprop(
+        "the number of shrinks is never never more than the sum of the number"
+        " of shrinks for the requested values",
+        []{
+            const auto shrinkable = testExecGen<5>()(Random(), 0);
 
-             int i = *gen::ranged<int>(0, 5);
-             const auto shrink = *seq::at(shrinkable.shrinks(), i);
-             const auto value = shrink.value().first;
-             int maxShrinks =
-                 std::accumulate(begin(value), end(value), 0);
-             RC_ASSERT(seq::length(shrink.shrinks()) <= maxShrinks);
-         });
+            int i = *newgen::inRange<int>(0, 5);
+            const auto shrink = *seq::at(shrinkable.shrinks(), i);
+            const auto value = shrink.value().first;
+            int maxShrinks =
+                std::accumulate(begin(value), end(value), 0);
+            RC_ASSERT(seq::length(shrink.shrinks()) <= maxShrinks);
+        });
 
-    prop("passes on the correct size",
-         [] {
-             int expectedSize = *gen::nonNegative<int>();
-             int n = *gen::ranged<int>(1, 10);
-             auto shrinkable = execRaw([=](const PassedSize &sz) {
-                 *genFixedCountdown(3); // Force some shrinks
-                 std::vector<int> sizes;
-                 sizes.push_back(sz.value);
-                 while (sizes.size() < n)
-                     sizes.push_back(*genSize());
-                 return sizes;
-             })(Random(), expectedSize);
+    newprop(
+        "passes on the correct size",
+        [] {
+            int expectedSize = *newgen::nonNegative<int>();
+            int n = *newgen::inRange<int>(1, 10);
+            auto shrinkable = execRaw([=](const PassedSize &sz) {
+                *genFixedCountdown(3); // Force some shrinks
+                std::vector<int> sizes;
+                sizes.push_back(sz.value);
+                while (sizes.size() < n)
+                    sizes.push_back(*genSize());
+                return sizes;
+            })(Random(), expectedSize);
 
-             auto valueShrinkable = shrinkable::map(
-                 shrinkable,
-                 [](std::pair<std::vector<int>, Recipe> &&x) {
-                     return std::move(x.first);
-                 });
+            auto valueShrinkable = shrinkable::map(
+                shrinkable,
+                [](std::pair<std::vector<int>, Recipe> &&x) {
+                    return std::move(x.first);
+                });
 
-             RC_ASSERT(
-                 shrinkable::all(
-                     valueShrinkable,
-                     [=](const Shrinkable<std::vector<int>> &x) {
-                         auto sizes = x.value();
-                         return std::all_of(
-                             begin(sizes), end(sizes),
-                             [=](int sz) { return sz == expectedSize; });
-                     }));
-         });
+            RC_ASSERT(
+                shrinkable::all(
+                    valueShrinkable,
+                    [=](const Shrinkable<std::vector<int>> &x) {
+                        auto sizes = x.value();
+                        return std::all_of(
+                            begin(sizes), end(sizes),
+                            [=](int sz) { return sz == expectedSize; });
+                    }));
+        });
 
-    prop("passes on sequentially split generators",
-         [](const Random &initial) {
-             int n = *gen::ranged<int>(1, 10);
-             auto shrinkable = execRaw([=](const PassedRandom &rnd) {
-                 std::vector<Random> randoms;
-                 randoms.push_back(rnd.value);
-                 while (randoms.size() < n)
-                     randoms.push_back(*genRandom());
-                 // Force some shrinks, must be last because it will steal a
-                 // random split otherwise
-                 *genFixedCountdown(3);
-                 return randoms;
-             })(initial, 0);
+    newprop(
+        "passes on sequentially split generators",
+        [](const Random &initial) {
+            int n = *newgen::inRange<int>(1, 10);
+            auto shrinkable = execRaw([=](const PassedRandom &rnd) {
+                std::vector<Random> randoms;
+                randoms.push_back(rnd.value);
+                while (randoms.size() < n)
+                    randoms.push_back(*genRandom());
+                // Force some shrinks, must be last because it will steal a
+                // random split otherwise
+                *genFixedCountdown(3);
+                return randoms;
+            })(initial, 0);
 
-             Random r(initial);
-             std::vector<Random> expected;
-             // The arguments are generated as a tuple so we want the same
-             // splitting behavior
-             expected.push_back(
-                 std::get<0>(newgen::tuple(genRandom())(r.split(), 0).value()));
-             while (expected.size() < n)
-                 expected.push_back(r.split());
+            Random r(initial);
+            std::vector<Random> expected;
+            // The arguments are generated as a tuple so we want the same
+            // splitting behavior
+            expected.push_back(
+                std::get<0>(newgen::tuple(genRandom())(r.split(), 0).value()));
+            while (expected.size() < n)
+                expected.push_back(r.split());
 
-             auto valueShrinkable = shrinkable::map(
-                 shrinkable,
-                 [](std::pair<std::vector<Random>, Recipe> &&x) {
-                     return std::move(x.first);
-                 });
+            auto valueShrinkable = shrinkable::map(
+                shrinkable,
+                [](std::pair<std::vector<Random>, Recipe> &&x) {
+                    return std::move(x.first);
+                });
 
-             RC_ASSERT(
-                 shrinkable::all(
-                     valueShrinkable,
-                     [=](const Shrinkable<std::vector<Random>> &x) {
-                         return x.value() == expected;
-                     }));
-         });
+            RC_ASSERT(
+                shrinkable::all(
+                    valueShrinkable,
+                    [=](const Shrinkable<std::vector<Random>> &x) {
+                        return x.value() == expected;
+                    }));
+        });
 
     SECTION("the ingredients of the recipe exactly match the generated value") {
         REQUIRE(
