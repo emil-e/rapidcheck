c75de7f5848c245fca6e74c5a57b3c3318f46b4a
Emil Eriksson <shadewind@gmail.com>
2015-04-06 17:02:23 +0200

    Move ExecTests to new framework

--- test/newgen/ExecTests.cpp
+++ test/newgen/ExecTests.cpp
@@ -11,27 +11,28 @@ using namespace rc::newgen::detail;
 using namespace rc::test;
 
 TEST_CASE("newgen::exec") {
-    prop("yields the same result as execRaw but without recipe",
-         [](const GenParams &params) {
-             using Tuple = std::tuple<int, int, int>;
+    newprop(
+        "yields the same result as execRaw but without recipe",
+        [](const GenParams &params) {
+            using Tuple = std::tuple<int, int, int>;
 
-             const auto callable = [](const FixedCountdown<2> &a) {
-                 return std::make_tuple(
-                     a.value,
-                     *genFixedCountdown(2),
-                     *genFixedCountdown(2));
-             };
+            const auto callable = [](const FixedCountdown<2> &a) {
+                return std::make_tuple(
+                    a.value,
+                    *genFixedCountdown(2),
+                    *genFixedCountdown(2));
+            };
 
-             const auto expected = newgen::map(
-                 execRaw(callable), [](const std::pair<Tuple, Recipe> &p) {
-                     return p.first;
-                 })(params.random, params.size);
+            const auto expected = newgen::map(
+                execRaw(callable), [](const std::pair<Tuple, Recipe> &p) {
+                    return p.first;
+                })(params.random, params.size);
 
-             const auto actual = newgen::exec(callable)(params.random,
-                                                        params.size);
+            const auto actual = newgen::exec(callable)(params.random,
+                                                       params.size);
 
-             RC_ASSERT(actual == expected);
-         });
+            RC_ASSERT(actual == expected);
+        });
 
     SECTION("works with non-copyable types") {
         auto shrinkable = newgen::exec([=](NonCopyable nc) {
