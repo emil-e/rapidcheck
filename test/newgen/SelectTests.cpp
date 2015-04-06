b5017e5b0d70606f16e53bc78d038cd51a9d2f34
Emil Eriksson <shadewind@gmail.com>
2015-04-06 17:16:27 +0200

    Move SelectTests to new framework

--- test/newgen/SelectTests.cpp
+++ test/newgen/SelectTests.cpp
@@ -20,11 +20,11 @@ struct ElementOfTests
     static void exec()
     {
         // TODO should be in standard library of generators!
-        static const auto nonEmpty = gen::suchThat<T>([] (const T &x) {
+        static const auto nonEmpty = newgen::suchThat<T>([](const T &x) {
             return !x.empty();
         });
 
-        templatedProp<T>(
+        newtemplatedProp<T>(
             "all generated elements are elements of the container",
             [](const GenParams &params) {
                 T elements = *nonEmpty;
@@ -34,7 +34,7 @@ struct ElementOfTests
                           end(elements));
             });
 
-        templatedProp<T>(
+        newtemplatedProp<T>(
             "all elements are eventually generated",
             [](const GenParams &params) {
                 T elements = *nonEmpty;
@@ -50,8 +50,8 @@ struct ElementOfTests
 
         TEMPLATED_SECTION(T, "throws GenerationFailure on empty container") {
             T container;
-            REQUIRE_THROWS_AS(newgen::elementOf(container)(Random(0), 0),
-                              GenerationFailure);
+            const auto shrinkable = newgen::elementOf(container)(Random(0), 0);
+            REQUIRE_THROWS_AS(shrinkable.value(), GenerationFailure);
         }
     }
 };
@@ -63,71 +63,77 @@ TEST_CASE("newgen::elementOf") {
 }
 
 TEST_CASE("newgen::element") {
-    prop("all generated elements are arguments",
-         [](const GenParams &params,
-            const std::string &a,
-            const std::string &b,
-            const std::string &c) {
-             const auto gen = newgen::element(a, b, c);
-             const auto x = gen(params.random, params.size).value();
-             RC_ASSERT((x == a) || (x == b) || (x == c));
-         });
+    newprop(
+        "all generated elements are arguments",
+        [](const GenParams &params,
+           const std::string &a,
+           const std::string &b,
+           const std::string &c) {
+            const auto gen = newgen::element(a, b, c);
+            const auto x = gen(params.random, params.size).value();
+            RC_ASSERT((x == a) || (x == b) || (x == c));
+        });
 
-    prop("all elements are eventually generated",
-         [](const GenParams &params,
-            const std::string &a,
-            const std::string &b,
-            const std::string &c) {
-             const auto gen = newgen::element(a, b, c);
-             std::set<std::string> all{a, b, c};
-             std::set<std::string> generated;
-             Random r(params.random);
-             while (all != generated)
-                 generated.insert(gen(r.split(), params.size).value());
-             RC_SUCCEED("All values generated");
-         });
+    newprop(
+        "all elements are eventually generated",
+        [](const GenParams &params,
+           const std::string &a,
+           const std::string &b,
+           const std::string &c) {
+            const auto gen = newgen::element(a, b, c);
+            std::set<std::string> all{a, b, c};
+            std::set<std::string> generated;
+            Random r(params.random);
+            while (all != generated)
+                generated.insert(gen(r.split(), params.size).value());
+            RC_SUCCEED("All values generated");
+        });
 }
 
 TEST_CASE("newgen::oneOf") {
-    prop("all generated elements come from one of the generators",
-         [](const GenParams &params,
-            const std::string &a,
-            const std::string &b,
-            const std::string &c) {
-             const auto gen = newgen::oneOf(newgen::just(a),
-                                            newgen::just(b),
-                                            newgen::just(c));
-             const auto x = gen(params.random, params.size).value();
-             RC_ASSERT((x == a) || (x == b) || (x == c));
-         });
+    newprop(
+        "all generated elements come from one of the generators",
+        [](const GenParams &params,
+           const std::string &a,
+           const std::string &b,
+           const std::string &c) {
+            const auto gen = newgen::oneOf(newgen::just(a),
+                                           newgen::just(b),
+                                           newgen::just(c));
+            const auto x = gen(params.random, params.size).value();
+            RC_ASSERT((x == a) || (x == b) || (x == c));
+        });
 
-    prop("all generators are eventually used",
-         [](const GenParams &params,
-            const std::string &a,
-            const std::string &b,
-            const std::string &c) {
-             const auto gen = newgen::oneOf(newgen::just(a),
-                                            newgen::just(b),
-                                            newgen::just(c));
-             std::set<std::string> all{a, b, c};
-             std::set<std::string> generated;
-             Random r(params.random);
-             while (all != generated)
-                 generated.insert(gen(r.split(), params.size).value());
-             RC_SUCCEED("All values generated");
-         });
+    newprop(
+        "all generators are eventually used",
+        [](const GenParams &params,
+           const std::string &a,
+           const std::string &b,
+           const std::string &c) {
+            const auto gen = newgen::oneOf(newgen::just(a),
+                                           newgen::just(b),
+                                           newgen::just(c));
+            std::set<std::string> all{a, b, c};
+            std::set<std::string> generated;
+            Random r(params.random);
+            while (all != generated)
+                generated.insert(gen(r.split(), params.size).value());
+            RC_SUCCEED("All values generated");
+        });
 
-    prop("passes the correct size to the generators",
-         [](const GenParams &params) {
-             const auto gen = newgen::oneOf(genPassedParams());
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value.size == params.size);
-         });
+    newprop(
+        "passes the correct size to the generators",
+        [](const GenParams &params) {
+            const auto gen = newgen::oneOf(genPassedParams());
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value.size == params.size);
+        });
 
-    prop("works with non-copyable types",
-         [](const GenParams &params) {
-             const auto gen = newgen::oneOf(newgen::arbitrary<NonCopyable>());
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(isArbitraryPredictable(value));
-         });
+    newprop(
+        "works with non-copyable types",
+        [](const GenParams &params) {
+            const auto gen = newgen::oneOf(newgen::arbitrary<NonCopyable>());
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(isArbitraryPredictable(value));
+        });
 }
