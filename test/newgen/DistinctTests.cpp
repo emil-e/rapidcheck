394c686c79fee3f6b212415ae8410d11c46c51a7
Emil Eriksson <shadewind@gmail.com>
2015-04-06 17:01:00 +0200

    Move DistinctTests to new framework

--- test/newgen/DistinctTests.cpp
+++ test/newgen/DistinctTests.cpp
@@ -12,27 +12,30 @@ using namespace rc;
 using namespace rc::test;
 
 TEST_CASE("newgen::distinctFrom") {
-    prop("value is never equal to the given one",
-         [](const GenParams &params, int x) {
-             const auto gen = newgen::distinctFrom(x);
-             onAnyPath(
-                 gen(params.random, params.size),
-                 [=](const Shrinkable<int> &value,
+    newprop(
+        "value is never equal to the given one",
+        [](const GenParams &params, int x) {
+            const auto gen = newgen::distinctFrom(x);
+            onAnyPath(
+                gen(params.random, params.size),
+                [=](const Shrinkable<int> &value,
                     const Shrinkable<int> &shrink) {
-                     RC_ASSERT(value.value() != x);
-                 });
-         });
+                    RC_ASSERT(value.value() != x);
+                });
+        });
 
-    prop("uses the correct generator when specified",
-         [](const GenParams &params, int x) {
-             const auto gen = newgen::distinctFrom(newgen::just(x), x - 1);
-             RC_ASSERT(gen(params.random, params.size).value() == x);
-         });
+    newprop(
+        "uses the correct generator when specified",
+        [](const GenParams &params, int x) {
+            const auto gen = newgen::distinctFrom(newgen::just(x), x - 1);
+            RC_ASSERT(gen(params.random, params.size).value() == x);
+        });
 
-    prop("uses the correct arbitrary instance if generator not specified",
-         [](const GenParams &params, const Predictable &x) {
-             const auto gen = newgen::distinctFrom(x);
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(isArbitraryPredictable(value));
-         });
+    newprop(
+        "uses the correct arbitrary instance if generator not specified",
+        [](const GenParams &params, const Predictable &x) {
+            const auto gen = newgen::distinctFrom(x);
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(isArbitraryPredictable(value));
+        });
 }
