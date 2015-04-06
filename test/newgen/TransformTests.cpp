fc91089bc8700cf1793682ddd6c8e0f0153bb82c
Emil Eriksson <shadewind@gmail.com>
2015-04-06 17:20:33 +0200

    Move TransformTests to new framework

--- test/newgen/TransformTests.cpp
+++ test/newgen/TransformTests.cpp
@@ -13,22 +13,24 @@ using namespace rc;
 using namespace rc::test;
 
 TEST_CASE("newgen::map") {
-    prop("maps the shrinkable returned by the generator",
-         [](const Shrinkable<int> &shrinkable) {
-             const auto mapper = [](int x) { return x * x; };
-             const auto mapped = newgen::map(
-                 Gen<int>(fn::constant(shrinkable)),
-                 mapper)(Random(), 0);
-             RC_ASSERT(shrinkable::map(shrinkable, mapper) == mapped);
-         });
+    newprop(
+        "maps the shrinkable returned by the generator",
+        [](const Shrinkable<int> &shrinkable) {
+            const auto mapper = [](int x) { return x * x; };
+            const auto mapped = newgen::map(
+                Gen<int>(fn::constant(shrinkable)),
+                mapper)(Random(), 0);
+            RC_ASSERT(shrinkable::map(shrinkable, mapper) == mapped);
+        });
 
-    prop("forwards the parameter to the generator",
-         [](const GenParams &params) {
-             const auto gen = newgen::map(
-                 genPassedParams(),
-                 [](GenParams &&x) { return std::move(x); });
-             RC_ASSERT(gen(params.random, params.size).value() == params);
-         });
+    newprop(
+        "forwards the parameter to the generator",
+        [](const GenParams &params) {
+            const auto gen = newgen::map(
+                genPassedParams(),
+                [](GenParams &&x) { return std::move(x); });
+            RC_ASSERT(gen(params.random, params.size).value() == params);
+        });
 
     SECTION("works with non-copyable types") {
         const auto value = newgen::map(
@@ -38,90 +40,99 @@ TEST_CASE("newgen::map") {
         REQUIRE(isArbitraryPredictable(value));
     }
 
-    prop("uses newgen::arbitrary if no generator is specified",
-         [](const GenParams &params) {
-             const auto value = newgen::map<Predictable>(
-                 [](Predictable &&x) { return std::move(x); })(
-                     params.random, params.size).value();
-             RC_ASSERT(isArbitraryPredictable(value));
-         });
+    newprop(
+        "uses newgen::arbitrary if no generator is specified",
+        [](const GenParams &params) {
+            const auto value = newgen::map<Predictable>(
+                [](Predictable &&x) { return std::move(x); })(
+                    params.random, params.size).value();
+            RC_ASSERT(isArbitraryPredictable(value));
+        });
 
-    prop("finds minimum where string represtation of unsigned integer must be"
-         " longer than some value",
-         [](const Random &random) {
-             const auto gen = newgen::map(
-                 newgen::arbitrary<unsigned int>(),
-                 [](unsigned int x) { return std::to_string(x); });
-             const auto n = *gen::ranged(2, 7);
-             std::string expected(n, '0');
-             expected[0] = '1';
-             const auto result = searchGen(
-                 random, gen::kNominalSize, gen,
-                 [=](const std::string &x) { return x.size() >= n; });
-             RC_ASSERT(result == expected);
-         });
+    newprop(
+        "finds minimum where string represtation of unsigned integer must be"
+        " longer than some value",
+        [](const Random &random) {
+            const auto gen = newgen::map(
+                newgen::arbitrary<unsigned int>(),
+                [](unsigned int x) { return std::to_string(x); });
+            const auto n = *newgen::inRange(2, 7);
+            std::string expected(n, '0');
+            expected[0] = '1';
+            const auto result = searchGen(
+                random, gen::kNominalSize, gen,
+                [=](const std::string &x) { return x.size() >= n; });
+            RC_ASSERT(result == expected);
+        });
 }
 
 TEST_CASE("newgen::cast") {
-    prop("casting to a larger type and then back yields original",
-         [](const Shrinkable<uint8_t> &shrinkable) {
-             const Gen<uint8_t> gen(fn::constant(shrinkable));
-             const auto cast = newgen::cast<uint8_t>(newgen::cast<int>(gen));
-             RC_ASSERT(cast(Random(), 0) == shrinkable);
-         });
+    newprop(
+        "casting to a larger type and then back yields original",
+        [](const Shrinkable<uint8_t> &shrinkable) {
+            const Gen<uint8_t> gen(fn::constant(shrinkable));
+            const auto cast = newgen::cast<uint8_t>(newgen::cast<int>(gen));
+            RC_ASSERT(cast(Random(), 0) == shrinkable);
+        });
 }
 
 TEST_CASE("newgen::suchThat") {
-    prop("generated value always matches predicate",
-         [](const GenParams &params) {
-             const auto gen = newgen::suchThat(
-                 newgen::arbitrary<int>(),
-                 [](int x) { return (x % 2) == 0; });
-             const auto shrinkable = gen(params.random, params.size);
-             onAnyPath(
-                 shrinkable,
-                 [](const Shrinkable<int> &value,
-                    const Shrinkable<int> &shrink) {
-                     RC_ASSERT((value.value() % 2) == 0);
-                 });
-         });
+    newprop(
+        "generated value always matches predicate",
+        [](const GenParams &params) {
+            const auto gen = newgen::suchThat(
+                newgen::arbitrary<int>(),
+                [](int x) { return (x % 2) == 0; });
+            const auto shrinkable = gen(params.random, params.size);
+            onAnyPath(
+                shrinkable,
+                [](const Shrinkable<int> &value,
+                   const Shrinkable<int> &shrink) {
+                    RC_ASSERT((value.value() % 2) == 0);
+                });
+        });
 
-    prop("if predicate returns true for every value, returned shrinkable is"
-         " unchanged",
-         [](const GenParams &params, const Shrinkable<int> &shrinkable) {
-             const Gen<int> underlying(fn::constant(shrinkable));
-             const auto gen = newgen::suchThat(underlying, fn::constant(true));
-             RC_ASSERT(underlying(params.random, params.size) ==
-                       gen(params.random, params.size));
-         });
+    newprop(
+        "if predicate returns true for every value, returned shrinkable is"
+        " unchanged",
+        [](const GenParams &params, const Shrinkable<int> &shrinkable) {
+            const Gen<int> underlying(fn::constant(shrinkable));
+            const auto gen = newgen::suchThat(underlying, fn::constant(true));
+            RC_ASSERT(underlying(params.random, params.size) ==
+                      gen(params.random, params.size));
+        });
 
-    prop("throws GenerationFailure if value cannot be generated",
-         [](const GenParams &params) {
-             const auto gen = newgen::suchThat(newgen::just<int>(0),
-                                               fn::constant(false));
-             try {
-                 gen(params.random, params.size);
-             } catch (const GenerationFailure &e) {
-                 RC_SUCCEED("Threw GenerationFailure");
-             }
-             RC_FAIL("Didn't throw GenerationFailure");
-         });
+    newprop(
+        "throws GenerationFailure if value cannot be generated",
+        [](const GenParams &params) {
+            const auto gen = newgen::suchThat(newgen::just<int>(0),
+                                              fn::constant(false));
+            const auto shrinkable = gen(params.random, params.size);
+            try {
+                shrinkable.value();
+            } catch (const GenerationFailure &e) {
+                RC_SUCCEED("Threw GenerationFailure");
+            }
+            RC_FAIL("Didn't throw GenerationFailure");
+        });
 
-    prop("passes the passed size to the underlying generator on the first try",
-         [] {
-             const auto size = *gen::ranged<int>(0, 2000);
-             const auto gen = newgen::suchThat(genPassedParams(),
-                                               fn::constant(true));
-             const auto params = gen(Random(), size).value();
-             RC_ASSERT(params.size == size);
-         });
+    newprop(
+        "passes the passed size to the underlying generator on the first try",
+        [] {
+            const auto size = *newgen::inRange<int>(0, 2000);
+            const auto gen = newgen::suchThat(genPassedParams(),
+                                              fn::constant(true));
+            const auto params = gen(Random(), size).value();
+            RC_ASSERT(params.size == size);
+        });
 
-    prop("uses newgen::arbitrary if no generator is specified",
-         [](const GenParams &params) {
-             const auto value = newgen::suchThat<Predictable>(
-                 fn::constant(true))(params.random, params.size).value();
-             RC_ASSERT(isArbitraryPredictable(value));
-         });
+    newprop(
+        "uses newgen::arbitrary if no generator is specified",
+        [](const GenParams &params) {
+            const auto value = newgen::suchThat<Predictable>(
+                fn::constant(true))(params.random, params.size).value();
+            RC_ASSERT(isArbitraryPredictable(value));
+        });
 
     SECTION("works with non-copyable types") {
         const auto value = newgen::suchThat(
@@ -132,86 +143,96 @@ TEST_CASE("newgen::suchThat") {
 }
 
 TEST_CASE("newgen::resize") {
-    prop("always uses the specified size",
-         [](const GenParams &params) {
-             const auto size = *gen::ranged<int>(0, 2000);
-             const auto gen = newgen::resize(size, genPassedParams());
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value.size == size);
-         });
+    newprop(
+        "always uses the specified size",
+        [](const GenParams &params) {
+            const auto size = *newgen::inRange<int>(0, 2000);
+            const auto gen = newgen::resize(size, genPassedParams());
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value.size == size);
+        });
 
-    prop("passes through random generator unchanged",
-         [](const GenParams &params) {
-             const auto gen = newgen::resize(0, genPassedParams());
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value.random == params.random);
-         });
+    newprop(
+        "passes through random generator unchanged",
+        [](const GenParams &params) {
+            const auto gen = newgen::resize(0, genPassedParams());
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value.random == params.random);
+        });
 }
 
 TEST_CASE("newgen::scale") {
-    prop("scales the size by the specified factor",
-         [](const GenParams &params) {
-             const auto gen = newgen::scale(2.0, genPassedParams());
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value.size == params.size * 2);
-         });
+    newprop(
+        "scales the size by the specified factor",
+        [](const GenParams &params) {
+            const auto gen = newgen::scale(2.0, genPassedParams());
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value.size == params.size * 2);
+        });
 
-    prop("passes through random generator unchanged",
-         [](const GenParams &params) {
-             const auto gen = newgen::scale(2.0, genPassedParams());
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value.random == params.random);
-         });
+    newprop(
+        "passes through random generator unchanged",
+        [](const GenParams &params) {
+            const auto gen = newgen::scale(2.0, genPassedParams());
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value.random == params.random);
+        });
 }
 
 TEST_CASE("newgen::noShrink") {
-    prop("returned shrinkable has expected value",
-         [](const GenParams &params, int x) {
-             const auto gen = newgen::noShrink(newgen::just(x));
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value == x);
-         });
+    newprop(
+        "returned shrinkable has expected value",
+        [](const GenParams &params, int x) {
+            const auto gen = newgen::noShrink(newgen::just(x));
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value == x);
+        });
 
-    prop("returned shrinkable has no shrinks",
-         [](const GenParams &params) {
-             const auto gen = newgen::noShrink(newgen::arbitrary<int>());
-             const auto shrinkable = gen(params.random, params.size);
-             RC_ASSERT(!shrinkable.shrinks().next());
-         });
+    newprop(
+        "returned shrinkable has no shrinks",
+        [](const GenParams &params) {
+            const auto gen = newgen::noShrink(newgen::arbitrary<int>());
+            const auto shrinkable = gen(params.random, params.size);
+            RC_ASSERT(!shrinkable.shrinks().next());
+        });
 
-    prop("passes generation params unchanged",
-         [](const GenParams &params) {
-             const auto gen = newgen::noShrink(genPassedParams());
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value == params);
-         });
+    newprop(
+        "passes generation params unchanged",
+        [](const GenParams &params) {
+            const auto gen = newgen::noShrink(genPassedParams());
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value == params);
+        });
 }
 
 TEST_CASE("newgen::withSize") {
-    prop("passes the current size to the callable",
-         [](const GenParams &params) {
-             const auto gen = newgen::withSize([](int size) {
-                 return newgen::just(size);
-             });
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value == params.size);
-         });
+    newprop(
+        "passes the current size to the callable",
+        [](const GenParams &params) {
+            const auto gen = newgen::withSize([](int size) {
+                return newgen::just(size);
+            });
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value == params.size);
+        });
 
-    prop("generates what the returned generator generates",
-         [](const GenParams &params, int x) {
-             const auto gen = newgen::withSize([=](int size) {
-                 return newgen::just(x);
-             });
-             const auto shrinkable = gen(params.random, params.size);
-             RC_ASSERT(shrinkable == shrinkable::just(x));
-         });
+    newprop(
+        "generates what the returned generator generates",
+        [](const GenParams &params, int x) {
+            const auto gen = newgen::withSize([=](int size) {
+                return newgen::just(x);
+            });
+            const auto shrinkable = gen(params.random, params.size);
+            RC_ASSERT(shrinkable == shrinkable::just(x));
+        });
 
-    prop("passes generation params unchanged",
-         [](const GenParams &params) {
-             const auto gen = newgen::withSize([](int size) {
-                 return genPassedParams();
-             });
-             const auto value = gen(params.random, params.size).value();
-             RC_ASSERT(value == params);
-         });
+    newprop(
+        "passes generation params unchanged",
+        [](const GenParams &params) {
+            const auto gen = newgen::withSize([](int size) {
+                return genPassedParams();
+            });
+            const auto value = gen(params.random, params.size).value();
+            RC_ASSERT(value == params);
+        });
 }
