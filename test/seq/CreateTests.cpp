f77a8af024aec6528ccb990c024ec56442e30d22
Emil Eriksson <shadewind@gmail.com>
2015-04-06 21:43:35 +0200

    Move seq/CreateTests to new framework

--- test/seq/CreateTests.cpp
+++ test/seq/CreateTests.cpp
@@ -14,25 +14,28 @@ using namespace rc;
 using namespace rc::test;
 
 TEST_CASE("seq::repeat") {
-    prop("repeatedly returns the given value",
-         [](const std::string &value) {
-             auto seq = seq::repeat(value);
-             for (int i = 0; i < 2000; i++) {
-                 auto x = seq.next();
-                 RC_ASSERT(x);
-                 RC_ASSERT(*x == value);
-             }
-         });
+    newprop(
+        "repeatedly returns the given value",
+        [](const std::string &value) {
+            auto seq = seq::repeat(value);
+            for (int i = 0; i < 2000; i++) {
+                auto x = seq.next();
+                RC_ASSERT(x);
+                RC_ASSERT(*x == value);
+            }
+        });
 
-    prop("copies are equal",
-         [](const std::string &value) {
-             assertEqualCopies(seq::take(200, seq::repeat(value)));
-         });
+    newprop(
+        "copies are equal",
+        [](const std::string &value) {
+            assertEqualCopies(seq::take(200, seq::repeat(value)));
+        });
 
-    prop("does not copy value on construction",
-         [](CopyGuard guard) {
-             auto seq = seq::repeat(std::move(guard));
-         });
+    newprop(
+        "does not copy value on construction",
+        [](CopyGuard guard) {
+            auto seq = seq::repeat(std::move(guard));
+        });
 }
 
 TEST_CASE("seq::just") {
@@ -43,25 +46,27 @@ TEST_CASE("seq::just") {
         seq.next();
     }
 
-    prop("returns the passed in objects",
-         [](const std::string &a,
-             const std::string &b,
-             const std::string &c)
-         {
-             auto seq = seq::just(a, b, c);
-             RC_ASSERT(*seq.next() == a);
-             RC_ASSERT(*seq.next() == b);
-             RC_ASSERT(*seq.next() == c);
-             RC_ASSERT(!seq.next());
-         });
+    newprop(
+        "returns the passed in objects",
+        [](const std::string &a,
+           const std::string &b,
+           const std::string &c)
+        {
+            auto seq = seq::just(a, b, c);
+            RC_ASSERT(*seq.next() == a);
+            RC_ASSERT(*seq.next() == b);
+            RC_ASSERT(*seq.next() == c);
+            RC_ASSERT(!seq.next());
+        });
 
-    prop("copies are equal",
-         [](const std::string &a,
-             const std::string &b,
-             const std::string &c)
-         {
-             assertEqualCopies(seq::just(a, b, c));
-         });
+    newprop(
+        "copies are equal",
+        [](const std::string &a,
+           const std::string &b,
+           const std::string &c)
+        {
+            assertEqualCopies(seq::just(a, b, c));
+        });
 }
 
 struct FromContainerTests
@@ -69,7 +74,7 @@ struct FromContainerTests
     template<typename T>
     static void exec()
     {
-        templatedProp<T>(
+        newtemplatedProp<T>(
             "the returned elements are equal to the elements"
             " returned by iterating",
             [](const T &elements) {
@@ -80,7 +85,7 @@ struct FromContainerTests
                 RC_ASSERT(!seq.next());
             });
 
-        templatedProp<T>(
+        newtemplatedProp<T>(
             "copies are equal",
             [](const T &elements) {
                 assertEqualCopies(seq::fromContainer(elements));
@@ -93,7 +98,7 @@ struct FromContainerCopyTests
     template<typename T>
     static void exec()
     {
-        templatedProp<T>(
+        newtemplatedProp<T>(
             "does not copy elements",
             [](T elements) {
                 auto seq = seq::fromContainer(std::move(elements));
@@ -116,77 +121,84 @@ TEST_CASE("seq::fromContainer") {
 }
 
 TEST_CASE("seq::fromIteratorRange") {
-    prop("creates a sequence that returns the values in the range",
-         [] (const std::vector<int> &elements) {
-             const int r1 = *gen::ranged<int>(0, elements.size() + 1);
-             const int r2 = *gen::ranged<int>(0, elements.size() + 1);
-             const int start = std::min(r1, r2);
-             const int end = std::max(r1, r2);
-             const auto startIt = elements.begin() + start;
-             const auto endIt = elements.begin() + end;
-             std::vector<int> subElements(startIt, endIt);
-             RC_ASSERT(seq::fromIteratorRange(startIt, endIt) ==
-                       seq::fromContainer(subElements));
-         });
+    newprop(
+        "creates a sequence that returns the values in the range",
+        [] (const std::vector<int> &elements) {
+            const int r1 = *newgen::inRange<int>(0, elements.size() + 1);
+            const int r2 = *newgen::inRange<int>(0, elements.size() + 1);
+            const int start = std::min(r1, r2);
+            const int end = std::max(r1, r2);
+            const auto startIt = elements.begin() + start;
+            const auto endIt = elements.begin() + end;
+            std::vector<int> subElements(startIt, endIt);
+            RC_ASSERT(seq::fromIteratorRange(startIt, endIt) ==
+                      seq::fromContainer(subElements));
+        });
 }
 
 TEST_CASE("seq::iterate") {
-    prop("returns an infinite sequence of applying the given function to the"
-         " value",
-         [](int start, int inc) {
-             const auto func = [=](int x) { return x + inc; };
-             auto seq = seq::iterate(start, func);
-             int x = start;
-             // Arbitrary number of iterations, sequence is infinite
-             for (int i = 0; i < 1000; i++) {
-                 RC_ASSERT(*seq.next() == x);
-                 x = func(x);
-             }
-         });
+    newprop(
+        "returns an infinite sequence of applying the given function to the"
+        " value",
+        [](int start, int inc) {
+            const auto func = [=](int x) { return x + inc; };
+            auto seq = seq::iterate(start, func);
+            int x = start;
+            // Arbitrary number of iterations, sequence is infinite
+            for (int i = 0; i < 1000; i++) {
+                RC_ASSERT(*seq.next() == x);
+                x = func(x);
+            }
+        });
 
-    prop("copies are equal",
-         [](int start, int inc) {
-             const auto func = [=](int x) { return x + inc; };
-             assertEqualCopies(seq::take(1000, seq::iterate(start, func)));
-         });
+    newprop(
+        "copies are equal",
+        [](int start, int inc) {
+            const auto func = [=](int x) { return x + inc; };
+            assertEqualCopies(seq::take(1000, seq::iterate(start, func)));
+        });
 }
 
 
 TEST_CASE("seq::range") {
-    auto smallInt = gen::scale(0.25, gen::arbitrary<int>());
+    static const auto smallInt = newgen::scale(0.25, newgen::arbitrary<int>());
 
-    prop("returns a sequence from start to end",
-         [&]{
-             int start = *smallInt;
-             int end = *smallInt;
+    newprop(
+        "returns a sequence from start to end",
+        []{
+            int start = *smallInt;
+            int end = *smallInt;
 
-             auto seq = seq::range(start, end);
-             int inc = (start < end) ? 1 : -1;
-             int i = start;
-             while (i != end) {
-                 auto x = seq.next();
-                 RC_ASSERT(x);
-                 RC_ASSERT(*x == i);
-                 i += inc;
-             }
-             RC_ASSERT(!seq.next());
-         });
+            auto seq = seq::range(start, end);
+            int inc = (start < end) ? 1 : -1;
+            int i = start;
+            while (i != end) {
+                auto x = seq.next();
+                RC_ASSERT(x);
+                RC_ASSERT(*x == i);
+                i += inc;
+            }
+            RC_ASSERT(!seq.next());
+        });
 
-    prop("copies are equal",
-         [&]{ assertEqualCopies(seq::range(*smallInt, *smallInt)); });
+    newprop(
+        "copies are equal",
+        [&]{ assertEqualCopies(seq::range(*smallInt, *smallInt)); });
 }
 
 TEST_CASE("seq::index") {
-    prop("returns an infinite sequence of increasing indexes from 0",
-         []{
-             auto seq = seq::index();
-             for (std::size_t i = 0; i < 2000; i++) {
-                 auto x = seq.next();
-                 RC_ASSERT(x);
-                 RC_ASSERT(*x == i);
-             }
-         });
+    newprop(
+        "returns an infinite sequence of increasing indexes from 0",
+        []{
+            auto seq = seq::index();
+            for (std::size_t i = 0; i < 2000; i++) {
+                auto x = seq.next();
+                RC_ASSERT(x);
+                RC_ASSERT(*x == i);
+            }
+        });
 
-    prop("copies are equal",
-         []{ assertEqualCopies(seq::take(2000, seq::index())); });
+    newprop(
+        "copies are equal",
+        []{ assertEqualCopies(seq::take(2000, seq::index())); });
 }
