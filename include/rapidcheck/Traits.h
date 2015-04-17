a0b256c6004da6ef70f3545c51e5a367a5ed5a0d
Emil Eriksson <shadewind@gmail.com>
2015-04-17 18:49:19 +0200

    Add All traits

--- include/rapidcheck/Traits.h
+++ include/rapidcheck/Traits.h
@@ -8,4 +8,16 @@ namespace rc {
 template<typename T>
 using Decay = typename std::decay<T>::type;
 
+//! Checks that all the types in `Ts` conforms to type trait `F`.
+template<template <typename> class F, typename ...Ts>
+struct All;
+
+template<template <class> class F>
+struct All<F> : public std::true_type {};
+
+template<template <class> class F, typename T, typename ...Ts>
+struct All<F, T, Ts...>
+    : public std::integral_constant<
+        bool, F<T>::value && All<F, Ts...>::value> {};
+
 } // namespace rc
