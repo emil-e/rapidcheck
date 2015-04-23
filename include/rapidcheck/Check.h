c8e972ae6aaa2f946e847be734c9c474804afde1
Emil Eriksson <shadewind@gmail.com>
2015-04-23 23:02:42 +0200

    Add rc::check for standalone operation

--- include/rapidcheck/Check.h
+++ include/rapidcheck/Check.h
@@ -18,6 +18,19 @@ template<typename Testable, typename ...Args>
 TestResult checkTestable(Testable &&testable, const Args &...args);
 
 } // namespace detail
+
+//! Checks the given testable and returns `true` on success and `false` on
+//! failure. This method will also print information about the testing to
+//! stderr.
+template<typename Testable>
+bool check(Testable &&testable);
+
+//! Same as `check(Testable &&)` but also takes a description of the property
+//! that is being tested as the first parameter. This will be used in the
+//! output.
+template<typename Testable>
+bool check(const std::string &description, Testable &&testable);
+
 } // namespace rc
 
 #include "Check.hpp"
--- include/rapidcheck/Check.hpp
+++ include/rapidcheck/Check.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 namespace rc {
 namespace detail {
 
@@ -12,4 +14,24 @@ TestResult checkTestable(Testable &&testable, const Args &...args)
 }
 
 } // namespace detail
+
+template<typename Testable>
+bool check(Testable &&testable)
+{ return check(std::string(), std::forward<Testable>(testable)); }
+
+template<typename Testable>
+bool check(const std::string &description, Testable &&testable)
+{
+    // Force loading of the configuration so that message comes _before_ the
+    // description
+    detail::defaultTestParams();
+
+    if (!description.empty())
+        std::cerr << std::endl << "- " << description << std::endl;
+    const auto result = detail::checkTestable(std::forward<Testable>(testable));
+    printResultMessage(result, std::cerr);
+    std::cerr << std::endl;
+    return result.template is<detail::SuccessResult>();
+}
+
 } // namespace rc
