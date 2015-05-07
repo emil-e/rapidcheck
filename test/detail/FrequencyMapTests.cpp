778a9ace7558a74b5b3c7bbd498606feb4cd4d2f
Emil Eriksson <shadewind@gmail.com>
2015-05-07 18:23:40 +0200

    Add FrequencyMap class

---
+++ test/detail/FrequencyMapTests.cpp
@@ -0,0 +1,42 @@
+#include <catch.hpp>
+#include <rapidcheck-catch.h>
+
+#include <numeric>
+
+#include "rapidcheck/detail/FrequencyMap.h"
+
+using namespace rc;
+using namespace rc::detail;
+
+TEST_CASE("FrequencyMap") {
+  static const auto genFrequencies = gen::suchThat(
+      gen::container<std::vector<std::size_t>>(gen::inRange(1, 10000)),
+      [](const std::vector<std::size_t> &x) { return !x.empty(); });
+
+  SECTION("sum") {
+    prop("returns sum of frequencies", [] {
+      const auto frequencies = *genFrequencies;
+      const auto expected =
+          std::accumulate(begin(frequencies), end(frequencies), 0);
+
+      RC_ASSERT(FrequencyMap(frequencies).sum() == expected);
+    });
+  }
+
+  SECTION("lookup") {
+    prop("equal to naive implementation",
+         [] {
+           const auto frequencies = *genFrequencies;
+           FrequencyMap map(frequencies);
+           const auto x = *gen::inRange<std::size_t>(0, map.sum());
+
+           std::size_t total = 0;
+           std::size_t i = 0;
+           for (i = 0; (total + frequencies[i]) <= x; i++) {
+             total += frequencies[i];
+           }
+
+           RC_ASSERT(map.lookup(x) == i);
+         });
+  }
+}
