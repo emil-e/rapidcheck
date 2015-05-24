26f52634a1ccc27748f274fc599aa150c8f6bb24
Emil Eriksson <shadewind@gmail.com>
2015-05-24 14:12:52 +0200

    Move CommandsGen to separate header file

---
+++ include/rapidcheck/state/detail/CommandsGen.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "rapidcheck/state/detail/Commands.h"
+
+namespace rc {
+namespace state {
+namespace detail {
+
+/// Create a commands generator with the given initial state and command
+/// generator factory function.
+template <typename Cmd, typename State, typename GenerationFunc>
+Gen<Commands<Cmd>> genCommands(State &&initialState, GenerationFunc &&genFunc);
+
+} // namespace detail
+} // namespace state
+} // namespace rc
+
+#include "CommandsGen.hpp"
