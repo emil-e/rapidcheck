2d9b4456bf9b7d02863abe227a09b1af0bcb3361
Emil Eriksson <shadewind@gmail.com>
2015-06-08 20:26:47 +0200

    Further reduce the verbosity of download scripts

--- ext/get_gmock.sh
+++ ext/get_gmock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -nv https://googlemock.googlecode.com/files/gmock-1.7.0.zip -O gmock-1.7.0.zip
-unzip gmock-1.7.0.zip
+unzip -q gmock-1.7.0.zip
 mv gmock-1.7.0 gmock
 rm gmock-1.7.0.zip
