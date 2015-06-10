9038b9b7c0c2a32d6955ccd0072cc3cbfef2ab3c
Emil Eriksson <shadewind@gmail.com>
2015-06-10 08:31:54 +0200

    Change implementation of anyCommands slightly

--- include/rapidcheck/state/State.hpp
+++ include/rapidcheck/state/State.hpp
@@ -13,10 +13,10 @@ namespace detail {
 template <typename Cmd,
           typename =
               typename std::is_constructible<Cmd, typename Cmd::State &&>::type>
-struct CommandMaker;
+struct MakeCommand;
 
 template <typename Cmd>
-struct CommandMaker<Cmd, std::true_type> {
+struct MakeCommand<Cmd, std::true_type> {
   static std::shared_ptr<const typename Cmd::CommandType>
   make(const typename Cmd::State &state) {
     return std::make_shared<Cmd>(state);
@@ -24,30 +24,10 @@ struct CommandMaker<Cmd, std::true_type> {
 };
 
 template <typename Cmd>
-struct CommandMaker<Cmd, std::false_type> {
+struct MakeCommand<Cmd, std::false_type> {
   static std::shared_ptr<const typename Cmd::CommandType>
   make(const typename Cmd::State &state) {
     return std::make_shared<Cmd>();
-  }
-};
-
-template <typename... Cmds>
-struct CommandPicker;
-
-template <typename Cmd>
-struct CommandPicker<Cmd> {
-  static std::shared_ptr<const typename Cmd::CommandType>
-  pick(const typename Cmd::State &state, int n) {
-    return CommandMaker<Cmd>::make(state);
-  }
-};
-
-template <typename Cmd, typename... Cmds>
-struct CommandPicker<Cmd, Cmds...> {
-  static std::shared_ptr<const typename Cmd::CommandType>
-  pick(const typename Cmd::State &state, int n) {
-    return (n == 0) ? CommandMaker<Cmd>::make(state)
-                    : CommandPicker<Cmds...>::pick(state, n - 1);
   }
 };
 
@@ -79,11 +59,17 @@ bool isValidCommand(const Command<State, Sut> &command, const State &s0) {
 template <typename Cmd, typename... Cmds>
 Gen<std::shared_ptr<const typename Cmd::CommandType>>
 anyCommand(const typename Cmd::State &state) {
+  using CmdSP = std::shared_ptr<const typename Cmd::CommandType>;
+  using State = typename Cmd::State;
+  using MakeFunc = CmdSP (*)(const State &);
+  MakeFunc makeFuncs[] = {&detail::MakeCommand<Cmd>::make,
+                          &detail::MakeCommand<Cmds>::make...};
+
   return [=](const Random &random, int size) {
     auto r = random;
     std::size_t n = r.split().next() % (sizeof...(Cmds) + 1);
     return rc::gen::exec([=] {
-      return detail::CommandPicker<Cmd, Cmds...>::pick(state, n);
+      return makeFuncs[n](state);
     })(r, size); // TODO monadic bind
   };
 }
