82798ac006c5f603aabfb59d8b9d65a4be56339d
Emil Eriksson <shadewind@gmail.com>
2015-08-22 13:25:24 +0200

    Add fixture support for GTest integration
    Fixes #56

--- examples/gtest/main.cpp
+++ examples/gtest/main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <rapidcheck/gtest.h>
 
+// Should succeed:
 RC_GTEST_PROP(MyTestCase,
               copyOfStringIsIdenticalToOriginal,
               (const std::string &str)) {
@@ -9,17 +10,54 @@ RC_GTEST_PROP(MyTestCase,
   RC_ASSERT(strCopy == str);
 }
 
-// Should obviously fail
+// Should obviously fail:
 RC_GTEST_PROP(MyTestCase, dividingByTenMakesAllNumbersEqual, (int a, int b)) {
   RC_ASSERT((a / 10) == (b / 10));
 }
 
-// If you don't have any arguments, you have to have an empty paren
+// If you don't have any arguments, you have to have an empty paren:
 RC_GTEST_PROP(MyTestCase, inRangeValueIsInRange, ()) {
   const auto range = *rc::gen::arbitrary<std::pair<int, int>>();
   const auto x = *rc::gen::inRange(range.first, range.second);
   RC_ASSERT(x >= range.first);
   RC_ASSERT(x < range.second);
+}
+
+// You can also create fixtures...
+class MyFixture : public ::testing::Test {
+protected:
+  MyFixture()
+      : counter(0) {}
+
+  void SetUp() override {
+    // SetUp works as usual...
+  }
+
+  void increment() { counter++; }
+
+  void TearDown() override {
+    // ...as does TearDown
+  }
+
+  int counter;
+};
+
+// ...and use them like this:
+RC_GTEST_FIXTURE_PROP(MyFixture,
+                      shouldInstantiateFixtureOnEachRun,
+                      (const std::vector<int> &ints)) {
+  for (const auto x : ints) {
+    increment();
+  }
+
+  RC_ASSERT(counter == ints.size());
+}
+
+// A normal Google test can use the same fixture:
+TEST_F(MyFixture, incrementIncrementsByOne) {
+  ASSERT_EQ(0, counter);
+  increment();
+  ASSERT_EQ(1, counter);
 }
 
 int main(int argc, char **argv) {
