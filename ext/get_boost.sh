e37bcbdf593871d4406641a2838d581ec40076df
Emil Eriksson <shadewind@gmail.com>
2015-08-31 11:24:31 +0200

    Upgrade to boost 1.59 to support VS 2015

--- ext/get_boost.sh
+++ ext/get_boost.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -s -L -o boost_1_58_0.tar.bz2 "http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.bz2?r=http%3A%2F%2Fwww.boost.org%2Fusers%2Fhistory%2Fversion_1_58_0.html&ts=1433786576&use_mirror=garr"
-tar xjfp boost_1_58_0.tar.bz2
-mv boost_1_58_0 boost
-rm boost_1_58_0.tar.bz2
+curl -s -L -o boost_1_59_0.tar.bz2 "http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.tar.bz2?r=http%3A%2F%2Fwww.boost.org%2Fusers%2Fhistory%2Fversion_1_59_0.html&ts=1441002160&use_mirror=netassist"
+tar xjfp boost_1_59_0.tar.bz2
+mv boost_1_59_0 boost
+rm boost_1_59_0.tar.bz2
