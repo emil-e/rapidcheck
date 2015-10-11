684f46a937355533ed16c7465d3107ce8498d219
Emil Eriksson <shadewind@gmail.com>
2015-10-11 22:52:56 +0200

    Add Random serialization

---
+++ include/rapidcheck/Random.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "rapidcheck/detail/Serialization.h"
+
+namespace rc {
+
+template <typename Iterator>
+Iterator serialize(const Random &random, Iterator output) {
+  using namespace rc::detail;
+  auto oit = output;
+  oit = serializeN(begin(random.m_key), random.m_key.size(), oit);
+  oit = serializeCompact(random.m_bits, oit);
+  oit = serializeCompact(random.m_counter, oit);
+  *oit = random.m_bitsi;
+  return ++oit;
+}
+
+template <typename Iterator>
+Iterator deserialize(Iterator begin, Iterator end, Random &output) {
+  using namespace rc::detail;
+  auto iit = begin;
+
+  iit = deserializeN<typename Random::Block::value_type>(
+      iit, end, output.m_key.size(), output.m_key.begin());
+  iit = deserializeCompact(iit, end, output.m_bits);
+
+  Random::Counter counter;
+  iit = deserializeCompact(iit, end, counter);
+  const auto blki = counter % std::tuple_size<Random::Block>::value;
+  // Normally, the block is calculated lazily if counter is divisible by 4 so
+  // let's simulate this.
+  if (blki != 0) {
+    // Calculate the block as if counter % 4 == 0
+    output.m_counter = counter - blki;
+    output.mash(output.m_block);
+  }
+  output.m_counter = counter;
+
+  output.m_bitsi = *iit;
+  return ++iit;
+}
+
+} // namespace rc
--- test/RandomTests.cpp
+++ test/RandomTests.cpp
@@ -10,6 +10,7 @@
 #include "util/Meta.h"
 
 using namespace rc;
+using namespace rc::detail;
 using namespace rc::test;
 
 namespace {
@@ -178,4 +179,35 @@ TEST_CASE("Random") {
   forEachType<AssociativeProperties,
               std::map<Random, int>,
               std::unordered_map<Random, int>>();
+
+  SECTION("serialize") {
+    prop("returns an iterator pointing past the written data",
+         [](const Random &random) {
+           std::vector<std::uint8_t> data;
+           // First serialize to expand the vector to the required size
+           serialize(random, std::back_inserter(data));
+           // Then again to get a comparable iterator
+           const auto it = serialize(random, begin(data));
+           RC_ASSERT(it == end(data));
+         });
+  }
+
+  SECTION("deserialize") {
+    prop("deserializes what was serialized",
+         [](const Random &random) {
+           std::vector<std::uint8_t> data;
+           serialize(random, std::back_inserter(data));
+           Random output;
+           const auto it = deserialize(begin(data), end(data), output);
+           RC_ASSERT(output == random);
+           RC_ASSERT(it == end(data));
+         });
+
+    SECTION("throws SerializationException on failure") {
+      std::vector<std::uint8_t> data;
+      Random output;
+      REQUIRE_THROWS_AS(deserialize(begin(data), end(data), output),
+                        SerializationException);
+    }
+  }
 }
