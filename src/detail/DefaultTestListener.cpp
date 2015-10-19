71c92f298ee33f85f31bb9d14b01498c00d52693
Emil Eriksson <shadewind@gmail.com>
2015-10-19 21:29:35 +0200

    Add ReproduceListener to default test listener

--- src/detail/DefaultTestListener.cpp
+++ src/detail/DefaultTestListener.cpp
@@ -1,14 +1,28 @@
 #include "DefaultTestListener.h"
 
 #include "LogTestListener.h"
+#include "ReproduceListener.h"
+#include "MulticastTestListener.h"
 
 namespace rc {
 namespace detail {
 
+namespace {
+
+template <typename T, typename... Args>
+std::unique_ptr<TestListener> makeListener(Args &&... args) {
+  return std::unique_ptr<TestListener>(new T(std::forward<Args>(args)...));
+}
+
+} // namespace
+
 std::unique_ptr<TestListener>
 makeDefaultTestListener(const Configuration &config, std::ostream &os) {
-  return std::unique_ptr<TestListener>(
-      new LogTestListener(os, config.verboseProgress, config.verboseShrinking));
+  MulticastTestListener::Listeners listeners;
+  listeners.push_back(makeListener<LogTestListener>(
+      os, config.verboseProgress, config.verboseShrinking));
+  listeners.push_back(makeListener<ReproduceListener>(os));
+  return makeListener<MulticastTestListener>(std::move(listeners));
 }
 
 TestListener &globalTestListener() {
--- test/detail/DefaultTestListenerTests.cpp
+++ test/detail/DefaultTestListenerTests.cpp
@@ -13,18 +13,20 @@ TEST_CASE("makeDefaultTestListener") {
       "false") {
     Configuration config;
     config.verboseProgress = false;
-    const auto listener = makeDefaultTestListener(config, os);
+    auto listener = makeDefaultTestListener(config, os);
     listener->onTestCaseFinished(CaseDescription());
+    listener.reset();
     REQUIRE(os.str().empty());
   }
 
   SECTION("should print . on test case success if verboseProgress == true") {
     Configuration config;
     config.verboseProgress = true;
-    const auto listener = makeDefaultTestListener(config, os);
+    auto listener = makeDefaultTestListener(config, os);
     CaseDescription desc;
     desc.result = CaseResult(CaseResult::Type::Success);
     listener->onTestCaseFinished(desc);
+    listener.reset();
     REQUIRE(os.str() == ".");
   }
 
@@ -32,18 +34,40 @@ TEST_CASE("makeDefaultTestListener") {
       "should not print anything on shrink tried if verboseShrinking == false") {
     Configuration config;
     config.verboseShrinking = false;
-    const auto listener = makeDefaultTestListener(config, os);
+    auto listener = makeDefaultTestListener(config, os);
     listener->onShrinkTried(CaseDescription(), false);
+    listener.reset();
     REQUIRE(os.str().empty());
   }
 
   SECTION("should print . on unaccepted shrink if verboseShrinking == true") {
     Configuration config;
     config.verboseShrinking = true;
-    const auto listener = makeDefaultTestListener(config, os);
+    auto listener = makeDefaultTestListener(config, os);
     CaseDescription desc;
     desc.result = CaseResult(CaseResult::Type::Success);
     listener->onShrinkTried(desc, false);
+    listener.reset();
     REQUIRE(os.str() == ".");
+  }
+
+  SECTION("should not have reproduce string if no failure") {
+    auto listener = makeDefaultTestListener(Configuration(), os);
+    TestMetadata metadata;
+    metadata.id = "foobar";
+    SuccessResult success;
+    listener->onTestFinished(metadata, success);
+    listener.reset();
+    REQUIRE(os.str().find("reproduce=") == std::string::npos);
+  }
+
+  SECTION("should have reproduce string on failure") {
+    auto listener = makeDefaultTestListener(Configuration(), os);
+    TestMetadata metadata;
+    metadata.id = "foobar";
+    FailureResult failure;
+    listener->onTestFinished(metadata, failure);
+    listener.reset();
+    REQUIRE(os.str().find("reproduce=") != std::string::npos);
   }
 }
