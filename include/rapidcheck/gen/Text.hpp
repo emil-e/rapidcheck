1b64beebc59f840a614688744459b7e2da8a30c4
Pierre Andersson <pierre.andersson01@gmail.com>
2016-03-16 21:09:23 +0100

    Added unicode generators

--- include/rapidcheck/gen/Text.hpp
+++ include/rapidcheck/gen/Text.hpp
@@ -5,12 +5,17 @@
 #include "rapidcheck/detail/BitStream.h"
 #include "rapidcheck/gen/Container.h"
 
+#include "rapidcheck/detail/Unicode.h"
+
 namespace rc {
 namespace gen {
 namespace detail {
 
 template <typename String>
 class StringGen;
+
+template <typename Container>
+class ContainerCodepointGen;
 
 template <typename T, typename... Args>
 class StringGen<std::basic_string<T, Args...>> {
@@ -42,6 +47,32 @@ public:
 };
 
 template <typename T, typename... Args>
+class ContainerCodepointGen<std::vector<T, Args...>> {
+public:
+	using Container = std::vector<T, Args...>;
+
+	Shrinkable<Container> operator()(const Random &random, int size) const {
+		auto stream = rc::detail::bitStreamOf(random);
+		Container str;
+		auto length = stream.next<std::size_t>() % (size + 1);
+		str.reserve(length);
+
+		for (std::size_t i = 0; i < length; i++) {
+			str.push_back(rc::detail::generateCodePoint<T>(stream));
+		}
+
+		return shrinkable::shrinkRecur(
+			std::move(str),
+			[](const Container &s) {
+			return seq::concat(shrink::removeChunks(s),
+				shrink::eachElement(s, &shrink::unicodeCodepoint<T>));
+		});
+	}
+};
+
+
+
+template <typename T, typename... Args>
 struct DefaultArbitrary<std::basic_string<T, Args...>> {
   static Gen<std::basic_string<T, Args...>> arbitrary() {
     return gen::string<std::basic_string<T, Args...>>();
@@ -64,10 +95,51 @@ Gen<T> character() {
   };
 }
 
+
+template <typename T>
+Gen<T> unicodeCodepoint() {
+	return [](const Random &random, int size) {
+		auto stream = ::rc::detail::bitStreamOf(random);
+
+		return shrinkable::shrinkRecur(rc::detail::generateCodePoint<T>(stream),
+			&shrink::unicodeCodepoint<T>);
+	};
+}
+
+template <typename Container>
+Gen<Container> unicodeCodepoints()
+{
+	return detail::ContainerCodepointGen<Container>();
+}
+
+template <typename String>
+Gen<String> characterUtf8() {
+	return map(unicodeCodepoint(), [](T codepoint)
+	{
+		return rc::detail::makeCharacterUtf8<String>(codepoint);
+	});
+}
+
+
 template <typename String>
 Gen<String> string() {
   return detail::StringGen<String>();
 }
+
+template <typename String>
+Gen<String> stringUtf8() {
+	return map(unicodeCodepoints<std::vector<uint32_t>>(), [](const std::vector<uint32_t>& codepoints)
+	{
+		String str;
+		for (const auto& cp : codepoints)
+		{
+			str += rc::detail::makeCharacterUtf8<String>(cp);
+		}
+		return std::move(str);
+	});
+}
+
+
 
 } // namespace gen
 } // namespace rc
--- test/gen/TextTests.cpp
+++ test/gen/TextTests.cpp
@@ -87,6 +87,114 @@ struct StringProperties {
   }
 };
 
+struct StringUtf8Properties {
+	template <typename T>
+	static void exec() {
+		templatedProp<T>("length is at most four times to size",
+			[](const GenParams &params) {
+			const auto gen = gen::stringUtf8<T>();
+			const auto shrinkable = gen(params.random, params.size);
+			RC_ASSERT(shrinkable.value().size() <=
+				static_cast<std::size_t>(params.size) * 4);
+		});
+
+		templatedProp<T>("first shrink is empty",
+			[](const GenParams &params) {
+			const auto gen = gen::stringUtf8<T>();
+			const auto shrinkable = gen(params.random, params.size);
+			RC_PRE(!shrinkable.value().empty());
+			RC_ASSERT(shrinkable.shrinks().next()->value().empty());
+		});
+
+		templatedProp<T>(
+			"the size of each shrink is the same or smaller than the original",
+			[](const GenParams &params) {
+			onAnyPath(
+				gen::stringUtf8<T>()(params.random, params.size),
+				[](const Shrinkable<T> &value, const Shrinkable<T> &shrink) {
+				RC_ASSERT(containerSize(shrink.value()) <=
+					containerSize(value.value()));
+			});
+		});
+
+		templatedProp<T>("none of the shrinks equal the original value",
+			[](const GenParams &params) {
+			onAnyPath(gen::stringUtf8<T>()(params.random, params.size),
+				[](const Shrinkable<T> &value,
+					const Shrinkable<T> &shrink) {
+				RC_ASSERT(value.value() != shrink.value());
+			});
+		});
+	}
+};
+
+struct CodepointContainerProperties {
+	template <typename T>
+	static void exec() {
+		templatedProp<T>("length is at most size",
+			[](const GenParams &params) {
+			const auto gen = gen::unicodeCodepoints<T>();
+			const auto shrinkable = gen(params.random, params.size);
+			RC_ASSERT(shrinkable.value().size() <=
+				static_cast<std::size_t>(params.size));
+		});
+
+		templatedProp<T>(
+			"finds minimum where container must be longer than a certain length",
+			[](const Random &random) {
+			const auto n = *gen::inRange<std::size_t>(0, 10);
+			const auto size = *gen::inRange<int>(50, 100);
+			const auto result =
+				searchGen(random,
+					size,
+					gen::unicodeCodepoints<T>(),
+					[=](const T &x) { return x.size() >= n; });
+			T expected(n, 'a');
+			RC_ASSERT(result == expected);
+		});
+
+		templatedProp<T>("first shrink is empty",
+			[](const GenParams &params) {
+			const auto gen = gen::unicodeCodepoints<T>();
+			const auto shrinkable = gen(params.random, params.size);
+			RC_PRE(!shrinkable.value().empty());
+			RC_ASSERT(shrinkable.shrinks().next()->value().empty());
+		});
+
+		templatedProp<T>(
+			"the size of each shrink is the same or smaller than the original",
+			[](const GenParams &params) {
+			onAnyPath(
+				gen::unicodeCodepoints<T>()(params.random, params.size),
+				[](const Shrinkable<T> &value, const Shrinkable<T> &shrink) {
+				RC_ASSERT(containerSize(shrink.value()) <=
+					containerSize(value.value()));
+			});
+		});
+
+		templatedProp<T>("none of the shrinks equal the original value",
+			[](const GenParams &params) {
+			onAnyPath(gen::unicodeCodepoints<T>()(params.random, params.size),
+				[](const Shrinkable<T> &value,
+					const Shrinkable<T> &shrink) {
+				RC_ASSERT(value.value() != shrink.value());
+			});
+		});
+	}
+};
+
+
 TEST_CASE("gen::string") {
   forEachType<StringProperties, std::string, std::wstring>();
 }
+
+TEST_CASE("gen::stringUtf8") {
+	forEachType<StringUtf8Properties, std::string, std::wstring>();
+}
+
+TEST_CASE("gen::unicodeCodepoints") {
+	forEachType<CodepointContainerProperties, std::vector<uint32_t>, std::vector<int64_t>>();
+}
+
+
+
