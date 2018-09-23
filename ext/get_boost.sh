7e8460db6aa134e7ecd9bd081ecf4717126ce61c
Michał Janiszewski <janisozaur+signed@gmail.com>
2018-09-23 22:58:19 +0200

    Update to boost 1.68

--- ext/get_boost.sh
+++ ext/get_boost.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -s -L -o boost_1_65_1.tar.bz2 https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.bz2
-tar xjfp boost_1_65_1.tar.bz2
-mv boost_1_65_1 boost
-rm boost_1_65_1.tar.bz2
+curl -s -L -o boost_1_68_0.tar.bz2 https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.bz2
+tar xjfp boost_1_68_0.tar.bz2
+mv boost_1_68_0 boost
+rm boost_1_68_0.tar.bz2
